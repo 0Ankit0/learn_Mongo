2cce529b583c88e11cc70152247dc93b238c288e
0ankit0 <ankitpaudel.1373@gmail.com>
2024-04-19 07:19:05 +0545

    initializing repo with basics and dummy data

---
+++ Mongodb_crud/Basics.mongodb.js
@@ -0,0 +1,14 @@
+//To show all the databases in the system
+show databases;
+//Declare the database to use
+use('test');
+//To show the current database
+db
+
+//Now you can use db for accessing the database
+
+//To create a collection
+db.createCollection('users');
+
+//To show all the collections in the database
+show collections;
